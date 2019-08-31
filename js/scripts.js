5936c3c1e7d28c5f8ee2c296b485b9581c3ec574
IKERRIZ <okothfaith94@gmail.com>
2019-08-31 15:04:40 +0300

    Add jquery to select and give total validation

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var totalCosts = [];
+
+function order(size, crust, meatyTopping, veggyTopping ){
+    this.size = size;
+    this.crust = crust;
+    this.meatyTopping = meatyTopping;
+    this.veggyTopping = veggyTopping;
+    this.price = 0;
+
+}
+
